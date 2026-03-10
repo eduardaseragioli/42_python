0ebb88c3c87ca241e41950dee758d4123035c6e9
Eduarda Seragioli Ulrick Ribeiro <eseragio@c1r2s3.42porto.com>
2026-03-10 12:19:58 +0000

    start mod6

---
+++ module_07/ex0/Card.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class Card(ABC):
+    def __init__(self, name: str, cost: int, rarity: str) -> None:
+        self.name = name
+        self.cost = cost
+        self.rarity = rarity
+
+    @abstractmethod
+    def play(self, game_state: dict) -> None:
+        pass
+
+    def get_card_info(self) -> dict:
+        card_info: dict = {
+            'name': self.name,
+            'cost': self.cost,
+            'rarity': self.rarity,
+            'type': type(self).__name__
+        }
+        return card_info
+
+    def is_playable(self, available_mana: int) -> None:
---
+++ module_07/ex0/CreatureCard.py
@@ -0,0 +1,3 @@
+def CreatureCard(Card) -> None:
+
+    def attack_target
