8dcee47146c00ffdaea42be7a39ff3d54f3851a8
Eduarda Seragioli Ulrick Ribeiro <eseragio@c1r3s3.42porto.com>
2026-04-13 19:36:11 +0100

    almost done ex0 mod10

--- module_10/ex0/lambda_spells.py
+++ module_10/ex0/lambda_spells.py
@@ -3,4 +3,23 @@ def artifact_sorter(artifacts: list[dict]) -> list[dict]:
     return sorter_artifact
 
 def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    filter_power = list(filter(lambda mage: mage["power"] >= min_power, mages))
+    return filter_power
     
+def spell_transformer(spells: list[str]) -> list[str]:
+    transformer_spell = list(map(lambda spell: "* " + spell + " *", spells))
+    return transformer_spell
+
+def mage_stats(mages: list[dict]) -> dict:
+    max_power =  max(mages, key=lambda x: x["power"])
+    min_power = min(mages, key=lambda x: x["power"])
+    average_power = round(sum(map(lambda x: x["power"], mages)) / len(mages), 2)
+
+    dict_result = {'max_power': max_power["power"], 'min_power': min_power["power"], 'avg_power': average_power}
+    return dict_result
+
+def main() -> None:
+    print("Testing artifact sorter...")
+
+if __name__ == "__main__":
+    main()
