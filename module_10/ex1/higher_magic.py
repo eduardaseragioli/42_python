d3ee37c18acc19df5725cb484f5a6a4980d88f9e
Eduarda Seragioli Ulrick Ribeiro <eseragio@c1r3s4.42porto.com>
2026-04-15 20:37:52 +0100

    duing ex1 mod10

--- module_10/ex1/higher_magic.py
+++ module_10/ex1/higher_magic.py
@@ -1,6 +1,19 @@
+from collections.abc import Callable
+
 def spell(target: str, power: int) -> str:
 
-def spell_combiner(spell1: Callable, spell2: Callable) -> Callable
-def power_amplifier(base_spell: Callable, multiplier: int) -> Callable
-def conditional_caster(condition: Callable, spell: Callable) -> Callable
-def spell_sequence(spells: list[Callable]) -> Callable
+def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
+    def combined(target: str, power: int) -> tuple:
+        return spell1(target, power), spell2(target, power)
+    return combined
+
+def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
+    def amplifier(target: str, power: int) -> Callable:
+        return base_spell(target, power * multiplier)
+    return amplifier
+
+def conditional_caster(condition: Callable, spell: Callable) -> Callable:
+    def  caster(target: str, power: int) -> Callable:
+        if condition
+
+def spell_sequence(spells: list[Callable]) -> Callable:
