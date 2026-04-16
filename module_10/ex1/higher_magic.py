1065d2869cecb4cb8c5b24ad571c5fb96fce1a24
Eduarda Seragioli Ulrick Ribeiro <eseragio@c1r3s4.42porto.com>
2026-04-16 19:22:33 +0100

    duing ex1 mod 10

--- module_10/ex1/higher_magic.py
+++ module_10/ex1/higher_magic.py
@@ -2,18 +2,28 @@ from collections.abc import Callable
 
 def spell(target: str, power: int) -> str:
 
+def heal(target: str, power: int) -> str:
+    return f"Heal restores {target} for {power} HP"
+
 def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
     def combined(target: str, power: int) -> tuple:
         return spell1(target, power), spell2(target, power)
     return combined
 
 def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
-    def amplifier(target: str, power: int) -> Callable:
+    def amplifier(target: str, power: int) -> str:
         return base_spell(target, power * multiplier)
     return amplifier
 
 def conditional_caster(condition: Callable, spell: Callable) -> Callable:
-    def  caster(target: str, power: int) -> Callable:
-        if condition
+    def caster(target: str, power: int) -> str:
+        if condition(target, power) is True:
+            return spell(target, power)
+        else:
+            return "Spell fizzled"
+    return caster
 
 def spell_sequence(spells: list[Callable]) -> Callable:
+    def sequence(target: str, power: int) -> list:
+        return [spell(target, power) for spell in spells]
+    return sequence
